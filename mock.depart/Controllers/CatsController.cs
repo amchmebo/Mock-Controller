0195eda2039d60d994ea6601b0654cecb6c40262
Jean-Michel Nadeau <jeanmichel.nadeau@gmail.com>
2023-11-01 01:31:33 -0400

    Init

---
+++ mock.depart/Controllers/CatsController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mock.depart.Data;
+using mock.depart.Models;
+using mock.depart.Services;
+
+namespace mock.depart.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CatsController : ControllerBase
+    {
+        private readonly CatsService _service;
+
+        public CatsController(CatsService service)
+        {
+            _service = service;
+        }
+
+        // DELETE: api/Cats/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteCat(int id)
+        {
+            // TODO vous devrez surement en faire une propriété pour pouvoir la "mock"
+            string? userid = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
+
+            // TODO vous devrez aussi faire un mock avec le service
+            Cat? cat = _service.Get(id);
+            if (cat == null)
+            {
+                return NotFound();
+            }
+            if (cat.CatOwner!.Id != userid)
+            {
+                return BadRequest("Cat is not yours");
+            }
+            if (cat.CuteLevel == Cuteness.BarelyOk)
+            {
+                cat = _service.Delete(id);
+                return Ok(cat);
+            }
+            else
+            {
+                return BadRequest("Cat is too cute");
+            }
+        }
+    }
+}
---
+++ mock.depart/Models/Cat.cs
@@ -0,0 +1,25 @@
+using System;
+using mock.depart.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.AspNetCore.Http.HttpResults;
+namespace mock.depart.Models
+{
+
+    public enum Cuteness
+    {
+        BarelyOk = 0,
+        YouCanKeepIt,
+        Amazing
+    }
+
+    public class Cat
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Cuteness CuteLevel { get; set; }
+        public virtual CatOwner? CatOwner { get; set; }
+    }
+
+}
+
